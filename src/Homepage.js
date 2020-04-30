6ef710a620df2140ddaae1f3610b0312d36d74d2
ravikirangambhire <ravikirangambhire599@gmail.com>
2020-04-30 21:19:27 +0530

    Added sign up
    Added Sign Up on sell button

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 import BuySearch from './components/Buy';
+import SignUp from './components/SignUp';
 import AnimalDetails from './components/AnimalDetails';
 import styles from './Homepage.css';
 /*export interface Homepage Props {
@@ -16,26 +17,34 @@ class Homepage  extends React.Component {
 		super(props);
 		this.state= {
 			home: true,
-			search: false
+			search: false,
+			signUp: false,
 		}
 	}
 	details= ()=> {
 		this.setState({search: true});
 		this.setState({home: false});
 	}
+	sellFlow = () => {
+		this.setState({home: false});
+		this.setState({signUp: true});
+	}
   render() { 
     return ( 
     <div>
-    	
+    	 
        { this.state.home? 
        	<div ><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
        		<button className= {styles.buy}onClick= {this.details}>Buy </button>
-    		<button> Sell</button>
+    		<button onClick= {this.sellFlow}> Sell</button>
        	</div>: ''
         }
     	{
     		this.state.search? <BuySearch />: ''
-    	}
+			}
+			{
+				this.state.signUp? <SignUp />: ''
+			}
     </div>
     );
   }
