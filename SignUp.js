051896dafe489277e832114466744b2c9fe28f93
ravikirangambhire <ravikirangambhire599@gmail.com>
2020-07-07 18:46:27 +0530

    Add files via upload
    added SignUp.js file with updated function.

--- SignUp.js
+++ SignUp.js
@@ -1,49 +1,76 @@
-import * as React from 'react';
+import React from 'react';
 import { Component } from 'react';
-import BuySearch from './components/Buy';
-import AnimalDetails from './components/AnimalDetails';
-//import SignUp  from './components/signUp';
-//import styles from './Homepage.css';
+import Navbar from './Navbar';
+    
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+        header:  "Animal Project"
+   }
+   handleSubmit= () =>{
+    console.log("Animal Project");
+    props.history.push("/Dashboard");
+}
+}
+   
 
-/*export interface Homepage Props {
-  
+    render() { 
+        return ( 
+         <div>
+            <Navbar />
+            <br/>
+            <div className= "row">
+                <div className = "col-sm-4"></div>
+                <div className = "card col-12 col-lg-4 login-card mt-2 hv-center">
+                    <form>
+                        <div className="form-group text-left">
+                        <label htmlFor="exampleInputEmail1">Name</label>
+                        <input type="Text" 
+                        className="form-control" 
+                        id="name" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter name"
+                        />
+                        </div>
+                        <div className="form-group text-left">
+                            <label htmlFor="exampleInputPassword1">Mobile Number</label>
+                            <input type="text"
+                                className="form-control" 
+                                id="mobileNumber" 
+                                placeholder="Mobile Number"
+                            />
+                        </div> 
+                    
+                        <div className="form-group text-left">
+                            <label htmlFor="exampleInputPassword1">Password</label>
+                            <input type="password" 
+                                className="form-control" 
+                                id="password" 
+                                placeholder="Password"
+                            />
+                        </div> 
+                        <div className="form-group text-left">
+                            <label htmlFor="exampleInputPassword1">Confirm Password</label>
+                            <input type="password" 
+                                className="form-control" 
+                                id="confirmPassword" 
+                                placeholder="Confirm Password"
+                            />
+                        </div>
+                        <button 
+                        type="submit" 
+                        className="btn btn-primary btn-block" onClick="handleSubmit"
+                    >Register   
+                    </button>
+                    </form>
+                </div>
+            </div>
+
+
+         </div>
+        );
+    }
 }
  
-export interface Homepage State {
-  
-}*/
- 
-class SignUp  extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state= {
-			
-			sign :true,
-			search:false
-		}
-	}
-	SignUp = ()=>{
-		this.setState({search:true});
-		this.setState({sign:false});
-	}
-	
-	
-  render() { 
-    return ( 
-    <div>
-    	
-       { this.state.sign? 
-       	<div ><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-       		
-    		<button className onClick= {this.SignUp}> Sell</button>
-       	</div>: ''
-        }
-    	{
-    		this.state.search? <BuySearch />: ''
-    	}
-    </div>
-    );
-  }
-}
- 
-export default SignUp ;
+export default SignUp;
